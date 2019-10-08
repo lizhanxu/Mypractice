14d3ecb2f7965be2b47e4d24f1f95c474da29414
lizhanxu <zhanxuli1997@163.com>
2019-10-08 18:00:04 +0800

    do commit

--- Spring/src/main/java/Interceptor/InterceptorImpl.java
+++ Spring/src/main/java/Interceptor/InterceptorImpl.java
@@ -11,17 +11,17 @@ import java.lang.reflect.Method;
 public class InterceptorImpl implements Interceptor {
     @Override
     public boolean before(Object proxy, Object target, Method method, Object[] args) {
-        System.err.println("反射方法前逻辑");
+        System.out.println("反射方法前逻辑");
         return false;//不反射被代理对象原有方法
     }
 
     @Override
     public void around(Object proxy, Object target, Method method, Object[] args) {
-        System.err.println("取代了被代理对象的方法");
+        System.out.println("取代了被代理对象的方法");
     }
 
     @Override
     public void after(Object proxy, Object target, Method method, Object[] args) {
-        System.err.println("反射方法后逻辑");
+        System.out.println("反射方法后逻辑");
     }
 }
