12e8f9becc36b48f7f36e2f52787988e552de8ff
lizhanxu <zhanxuli1997@163.com>
2019-10-12 17:50:12 +0800

    do commit

--- JavaBase/src/main/java/IO/IOTest.java
+++ JavaBase/src/main/java/IO/IOTest.java
@@ -10,6 +10,16 @@ import java.io.*;
  */
 public class IOTest {
     public static void main(String[] args) {
+
+        try (FileReader fileReader = new FileReader("JavaBase/src/main/java/IO/Readme.md"))
+        {
+            char[] buff = new char[9];
+            fileReader.read(buff);
+            System.out.println(buff);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         try (FileInputStream fin = new FileInputStream("JavaBase/src/main/java/IO/Readme.md"))
         {
             byte[] buff = new byte[7];
--- Spring/src/main/java/AOP/AOPConfig.java
+++ Spring/src/main/java/AOP/AOPConfig.java
@@ -1,7 +1,6 @@
 package AOP;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
@@ -10,7 +9,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  * @Date 2019/10/12
  * @Created by lizhanxu
  */
-@ComponentScan("AOP")
+@ComponentScan//默认扫描当前包
 @EnableAspectJAutoProxy//想要成功初始化Context需要引入aspectjweaver依赖，提供织入支持
 public class AOPConfig {
 }
